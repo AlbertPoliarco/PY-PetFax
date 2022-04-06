1c4953e75d1d4e1fb6db34ca411fcbee99161ea9
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-04-05 23:32:59 -0500

    displayed data

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,7 +1,11 @@
+import json
+
+pets = json.load(open('pets.json'))
+print(pets)
 from flask import Blueprint, render_template
 
 bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 @bp.route('/')
 def index():
-   return render_template('index.html')
+   return render_template("index.html", pets=pets)
